2cacddb5d310e14ffc0a547d7d12da8374a1ada4
Rodrigo Alviani <rodrigoalviani@gmail.com>
2015-01-21 14:28:26 -0200

    Fix multiple calls bug

--- midas-touch.js
+++ midas-touch.js
@@ -1,15 +1,16 @@
 'use strict';
 
 $(document).ready(function () {
-  $('*').click(function () {
-    var ouro = [
+  $(document).on('click', function (e) {
+    e.preventDefault();
+    var gold = [
       'http://www.4khdwallpapers.com/wp-content/uploads/2014/04/gold-texture-wallpaper-7.jpg',
       'http://images.all-free-download.com/images/graphiclarge/golden_texture_hd_picture_1_170580.jpg',
       'http://images.all-free-download.com/images/graphiclarge/gold_textured_background_hd_picture_3_169654.jpg'
     ];
-    $(this).css('background-image', 'url(' + Math.floor((Math.random() * ouro.length)+1) + ')');
-    $(this).css('background-position', 'center center');
-    $(this).css('background-size', 'cover');
-    $(this).css('color', '#fff)');
+    $(arguments[0].target).css('background-image', 'url(' + gold[Math.floor((Math.random() * gold.length))] + ')');
+    $(arguments[0].target).css('background-position', 'center center');
+    $(arguments[0].target).css('background-size', 'cover');
+    $(arguments[0].target).css('color', '#fff)');
   });
 });
