a436d3515cb4f355d7085c14717f7490dd05e9b6
Rodrigo Alviani <rodrigoalviani@gmail.com>
2015-01-21 12:33:27 -0200

    Let code scalable

--- midas-touch.js
+++ midas-touch.js
@@ -1,14 +1,15 @@
+'use strict';
+
 $(document).ready(function () {
-  var proxOuro = 0;
   $('*').click(function () {
-    var ouro = ["http://www.4khdwallpapers.com/wp-content/uploads/2014/04/gold-texture-wallpaper-7.jpg", "http://images.all-free-download.com/images/graphiclarge/golden_texture_hd_picture_1_170580.jpg", "http://images.all-free-download.com/images/graphiclarge/gold_textured_background_hd_picture_3_169654.jpg"];
-    $(this).css('background-image', 'url(' + ouro[proxOuro] + ')');
+    var ouro = [
+      'http://www.4khdwallpapers.com/wp-content/uploads/2014/04/gold-texture-wallpaper-7.jpg',
+      'http://images.all-free-download.com/images/graphiclarge/golden_texture_hd_picture_1_170580.jpg',
+      'http://images.all-free-download.com/images/graphiclarge/gold_textured_background_hd_picture_3_169654.jpg'
+    ];
+    $(this).css('background-image', 'url(' + Math.floor((Math.random() * ouro.length)+1) + ')');
     $(this).css('background-position', 'center center');
     $(this).css('background-size', 'cover');
     $(this).css('color', '#fff)');
-    proxOuro++;
-    if (proxOuro == 3) {
-      proxOuro = 0;
-    }
   });
 });
